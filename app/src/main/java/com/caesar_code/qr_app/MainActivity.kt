662f01320d6f43387f633b43955d4bec511596a2
MikHail19889 <m.v.myzhetsky@gmail.com>
2022-02-05 22:43:58 +0700

    QR

--- app/src/main/java/com/caesar_code/qr_app/MainActivity.kt
+++ app/src/main/java/com/caesar_code/qr_app/MainActivity.kt
@@ -5,19 +5,21 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import android.content.pm.ActivityInfo
 import android.content.pm.PackageManager
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
-
+import org.w3c.dom.Text
 
 
 class MainActivity : AppCompatActivity() {
     private lateinit var pLauncher: ActivityResultLauncher<Array<String>>
+    var res: String? = null
     @SuppressLint("SourceLockedOrientationActivity")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,6 +27,13 @@ class MainActivity : AppCompatActivity() {
         requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
         registerPermissionListener()
         checkCameraPermission()
+
+        val textView: TextView = findViewById(R.id.textView)
+        res = intent.extras?.getString("data").toString()
+        if(res == "null"){
+            res = "Please scanning QR or Bar code"
+        }
+        textView.setText(res).toString()
 
         val scanner: Button = findViewById(R.id.scanner)
         scanner.setOnClickListener {
@@ -38,6 +47,7 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
     }
+
     private fun checkCameraPermission(){
         when (PackageManager.PERMISSION_GRANTED) {
             ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
--- app/src/main/java/com/caesar_code/qr_app/ScanActivity.kt
+++ app/src/main/java/com/caesar_code/qr_app/ScanActivity.kt
@@ -1,13 +1,17 @@
 package com.caesar_code.qr_app
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import me.dm7.barcodescanner.zbar.Result
 import me.dm7.barcodescanner.zbar.ZBarScannerView
+import com.caesar_code.qr_app.MainActivity as MainActivity1
+
 
 class ScanActivity : AppCompatActivity(), ZBarScannerView.ResultHandler {
     private lateinit var zbView: ZBarScannerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         zbView = ZBarScannerView(this)
@@ -26,7 +30,9 @@ class ScanActivity : AppCompatActivity(), ZBarScannerView.ResultHandler {
     }
 
     override fun handleResult(result: Result?) {
-        Log.d("MyLog","Result:${result?.contents}")
-        finish()
+        val intent = Intent(applicationContext, MainActivity1::class.java)
+        intent.putExtra("data", result?.contents)
+        //Log.d("MyLog","Result:${result?.contents}")
+        startActivity(intent)
     }
 }
